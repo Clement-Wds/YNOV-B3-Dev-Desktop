a84ec70650e47a3d4de3b653dcdeb2b877240396
Clément Wds <clementwalshdeserrant@outlook.com>
2022-02-15 12:23:19 +0100

    timer ball movement

--- src/main/java/com/example/devdeskstopb3/HelloApplication.java
+++ src/main/java/com/example/devdeskstopb3/HelloApplication.java
@@ -1,13 +1,16 @@
 package com.example.devdeskstopb3;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 import java.io.IOException;
 
@@ -26,7 +29,8 @@ public class HelloApplication extends Application {
     static int rec2PosX = 750;
     static int rec2PosY = 50;
 
-    static int ballSize = 50;
+    static int ballWidth = 50;
+    static int ballHeight = 50;
     static int ballPosX = 400;
     static int ballPosY = 400;
 
@@ -34,11 +38,18 @@ public class HelloApplication extends Application {
     static String rec2Color = "GREEN";
     static String ballColor = "RED";
 
+    Timeline tl;
+    Rectangle ball;
+
+    public static void main(String[] args) {
+        launch();
+    }
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
 
-        Circle ball = new Circle(ballPosX, ballPosY, ballSize);
+        ball = new Rectangle(ballPosX, ballPosY, ballWidth, ballHeight);
 
         Rectangle r1 = new Rectangle(rec1PosX, rec1PosY, recHeight, recWidth);
         Rectangle r2 = new Rectangle(rec2PosX, rec2PosY, recHeight, recWidth);
@@ -49,10 +60,21 @@ public class HelloApplication extends Application {
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
+        run();
 
     }
 
-    public static void main(String[] args) {
-        launch();
+    public void run() {
+        //On utilise timer pour le mouvement automatique de la balle
+        tl = new Timeline();
+
+        tl.setCycleCount(Timeline.INDEFINITE);
+        tl.getKeyFrames().addAll(
+                new KeyFrame(Duration.millis(1000),
+                        new KeyValue(ball.translateXProperty(), ball.getX() + ball.getWidth())
+                )
+        );
+
+        tl.play();
     }
 }
